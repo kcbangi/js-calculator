7317ce12aa6f8a3d0127a6612f39d7ecd60d4741
Keith <kayzee@live.com>
2019-06-21 16:07:42 -1000

    almost finish

--- calculator.js
+++ calculator.js
@@ -6,6 +6,9 @@
  * @return {object} `calculator` object that can be used
  */
 
+ function calculatorModule() {
+     var total = 0;
+     var memory = 0;
 
   /**
    * sets the `total` to the number passed in
@@ -13,53 +16,108 @@
    * @return { Number }    current total
    */
 
-
+    var load = function(x) {
+        if(typeof x === 'number') {
+            total = x;
+            return total;
+        } 
+    };
+ 
   /**
    * Return the value of `total`
    * @return { Number }
    */
 
+    var getTotal = function() {
+        return total;
+    };
 
   /**
    * Sums the value passed in with `total`
    * @param { Number } x
    */
 
+   var add = function(x) {
+        if(typeof x === 'number') {
+            total += x;
+            return total;
+        }
+   };
 
   /**
    * Subtracts the value passed in from `total`
    * @param  { Number } x
    */
-
+   var subtract = function(x) {
+       if(typeof x === 'number') {
+            total -= x;
+            return total;
+       }
+   };
 
   /**
    * Multiplies the value by `total`
    * @param  { Number } x
    */
-
+   var multiply = function(x) {
+        if(typeof x === 'number') {
+            total *= x;
+            return total;
+        }
+   };
 
   /**
    * Divides the value passing in by `total`
    * @param  { Number } x
    */
 
+   var divide = function(x) {
+       if(typeof x === 'number') {
+           total /= x;
+           return total;
+       }
+   };
 
   /**
    * Return the value stored at `memory`
    * @return { Number }
    */
 
+   var recallMemory = function(x) {
+        x = memory;
+        return x;
+   };
 
   /**
    * Stores the value of `total` to `memory`
    */
 
+   var saveMemory = function() {
+
+        total = memory;
+   };
 
   /**
    * Clear the value stored at `memory`
    */
 
+   function clearMemory() {
+
+   };
   /**
    * Validation
    */
+
+  return {
+        load: load,
+        getTotal: getTotal,
+        add: add,
+        subtract: subtract,
+        multiply: multiply,
+        divide: divide,
+        recallMemory: recallMemory,
+        saveMemory: saveMemory,
+        clearMemory: clearMemory
+    };
+};
 
